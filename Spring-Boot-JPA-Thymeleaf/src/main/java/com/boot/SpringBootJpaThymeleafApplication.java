bb4fa264262e2c21bc3d60d78eabc7df20112521
杨智文 <yangzhiwen@chalco-steering.com>
2018-11-19 11:44:17 +0800

    init jpa thymeleaf

---
+++ Spring-Boot-JPA-Thymeleaf/src/main/java/com/boot/SpringBootJpaThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaThymeleafApplication.class, args);
+	}
+}
