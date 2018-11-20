2ccc80b16fa175a04dd9169c38556c06758c4be0
杨智文 <yangzhiwen@chalco-steering.com>
2018-11-20 18:18:00 +0800

    再搞一个Spring-boot-admin监控

---
+++ Spring-Boot-Admin/src/main/java/com/boot/admin/SpringBootAdminApplication.java
@@ -0,0 +1,12 @@
+package com.boot.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+}
