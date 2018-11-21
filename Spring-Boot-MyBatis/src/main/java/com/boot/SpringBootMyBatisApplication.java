12cde3471908b60c59c822eb1373b156c4f0a532
杨智文 <yangzhiwen@chalco-steering.com>
2018-11-21 14:45:52 +0800

    init Spring Boot MyBatis

---
+++ Spring-Boot-MyBatis/src/main/java/com/boot/SpringBootMyBatisApplication.java
@@ -0,0 +1,12 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyBatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyBatisApplication.class, args);
+	}
+}
