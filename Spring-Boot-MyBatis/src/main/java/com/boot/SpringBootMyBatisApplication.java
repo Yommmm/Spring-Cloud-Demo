dfdf51a6dd0b80edc0a801eca91f63d16f2959cc
杨智文 <yangzhiwen@chalco-steering.com>
2018-11-23 16:23:21 +0800

    接入Eureka，踩了个16年踩过的坑，defaultZone又写成default-zone，导致注册不上，这个eureka的yaml解析坑死了

--- Spring-Boot-MyBatis/src/main/java/com/boot/SpringBootMyBatisApplication.java
+++ Spring-Boot-MyBatis/src/main/java/com/boot/SpringBootMyBatisApplication.java
@@ -3,9 +3,11 @@ package com.boot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.boot.mapper")
+@EnableEurekaClient
 public class SpringBootMyBatisApplication {
 
 	public static void main(String[] args) {
