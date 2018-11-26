82d8b88ac86fa48b929cf11e84cf32c4cf5622f9
yommmm <1529133840@qq.com>
2018-11-26 22:15:24 +0800

    initial spring boot admin server and a client

--- Spring-Boot-Admin/src/main/java/com/boot/admin/SpringBootAdminApplication.java
+++ Spring-Boot-Admin/src/main/java/com/boot/admin/SpringBootAdminApplication.java
@@ -3,7 +3,10 @@ package com.boot.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
+@EnableAdminServer
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
