bc742c7e67a3b5c557d6548a3ef9b4eef4ae331b
杨智文 <yangzhiwen@chalco-steering.com>
2018-11-29 16:41:45 +0800

    加入swagger

--- Spring-Boot-MyBatis/src/main/java/com/boot/SpringBootMyBatisApplication.java
+++ Spring-Boot-MyBatis/src/main/java/com/boot/SpringBootMyBatisApplication.java
@@ -5,14 +5,44 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.boot.mapper")
 @EnableEurekaClient
 @EnableFeignClients
+@EnableSwagger2
 public class SpringBootMyBatisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMyBatisApplication.class, args);
 	}
+	
+	@Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.boot.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("测试Swagger")
+                .description("powered by By-Health")
+                .termsOfServiceUrl("http://www.by-health.com/")
+                .version("1.0")
+                .build();
+    }
+    
 }
