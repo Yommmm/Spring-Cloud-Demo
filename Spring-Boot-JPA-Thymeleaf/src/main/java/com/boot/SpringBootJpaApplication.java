718a74ec05eb3d4f862d5d392f63eba062b6c538
杨智文 <yangzhiwen@chalco-steering.com>
2018-12-04 15:56:27 +0800

    JPA 接入监控

--- Spring-Boot-JPA-Thymeleaf/src/main/java/com/boot/SpringBootJpaApplication.java
+++ Spring-Boot-JPA-Thymeleaf/src/main/java/com/boot/SpringBootJpaApplication.java
@@ -2,11 +2,41 @@ package com.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaApplication.class, args);
 	}
+	
+	@Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.boot.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("测试Swagger")
+                .description("powered by Yommmm")
+                .termsOfServiceUrl("http://yommmm.github.com/")
+                .version("1.0")
+                .build();
+    }
+    
 }
