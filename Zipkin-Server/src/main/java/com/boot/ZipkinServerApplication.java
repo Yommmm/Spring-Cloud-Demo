4ce32e1fa4c386439e0a2a7c5d745863dce9a818
杨智文 <yangzhiwen@chalco-steering.com>
2018-12-05 17:35:42 +0800

    只能吧zipkin的sb版本降到1.5.18

--- Zipkin-Server/src/main/java/com/boot/ZipkinServerApplication.java
+++ Zipkin-Server/src/main/java/com/boot/ZipkinServerApplication.java
@@ -2,8 +2,13 @@ package com.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
