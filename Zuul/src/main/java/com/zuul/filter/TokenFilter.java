0a5b5979cbb2a5989f6e1ab139194212fdd42f4e
杨智文 <yangzhiwen@chalco-steering.com>
2018-12-10 16:11:58 +0800

    get traceId and put it in response message

--- Zuul/src/main/java/com/zuul/filter/TokenFilter.java
+++ Zuul/src/main/java/com/zuul/filter/TokenFilter.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -24,31 +25,49 @@ public class TokenFilter extends ZuulFilter {
 	public Object run() throws ZuulException {
 		logger.info("证明下进来过这个过滤器~");
 		
-		RequestContext requestContext = RequestContext.getCurrentContext();
-		HttpServletRequest request = requestContext.getRequest();
+		RequestContext ctx = RequestContext.getCurrentContext();
+		/**
+		 * 链路跟踪的traceId放进请求头
+		 * 由于Zipkin会在每个Http请求中携带traceId，在此不再往HttpServletRequest中放
+		 * 在统一异常处理的框架，或者再统一放返回格式中
+		 * Successed
+		 * {
+		 *   "data": null,
+		 *   "traceId": "traceId",
+		 *   "resultCode": "200",
+		 *   "resutlMsg": "success"
+		 * }
+		 * 
+		 * failed
+		 * {
+		 *   "errorMsg": null,
+		 *   "traceId": "traceId",
+		 *   "resultCode": "500",
+		 *   "resutlMsg": "failed"
+		 * }
+		 */
+		ctx.addZuulRequestHeader("trace_id", MDC.get("traceId"));
 		
-		logger.info("--->>> TokenFilter {},{}", request.getMethod(), request.getRequestURL().toString());
-
-        String accessToken = request.getHeader("accessToken");
-        accessToken = null == accessToken ? request.getParameter("token") : accessToken;
+		HttpServletRequest request = ctx.getRequest();
+		logger.info("TokenFilter {}, {}", request.getMethod(), request.getRequestURL().toString());
         
         /**
-         * TODO
          * 1.用户登录时，可将认证完成之后取得的AccessToken放入缓存
          * 2.缓存中以token为键，该用户拥有的资源为值，并设置有效期，前端将所有信息写入浏览器缓存
          * 3.网关可通过token进行鉴权，确定当前用户拥有的权限
          */
-
+		String accessToken = request.getHeader("accessToken");
+		accessToken = null == accessToken ? request.getParameter("token") : accessToken;
         if (!StringUtils.isEmpty(accessToken)) {
-        	requestContext.setSendZuulResponse(true); //对请求进行路由
-        	requestContext.setResponseStatusCode(200);
-        	requestContext.set("isSuccess", true);
+        	ctx.setSendZuulResponse(true); //对请求进行路由
+        	ctx.setResponseStatusCode(200);
+        	ctx.set("isSuccess", true);
             return null;
         } else {
-        	requestContext.setSendZuulResponse(false); //不对其进行路由
-        	requestContext.setResponseStatusCode(400);
-        	requestContext.setResponseBody("accessToken is empty");
-            requestContext.set("isSuccess", false);
+        	ctx.setSendZuulResponse(false); //不对其进行路由
+        	ctx.setResponseStatusCode(400);
+        	ctx.setResponseBody("accessToken is empty");
+        	ctx.set("isSuccess", false);
             return null;
         }
 		
