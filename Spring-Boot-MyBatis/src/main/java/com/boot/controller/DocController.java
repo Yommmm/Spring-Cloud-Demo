03b6c0e98ff9d6b71feb12e4c7bfd0708b58e6da
杨智文 <yangzhiwen@chalco-steering.com>
2018-12-12 15:59:39 +0800

    今天已经没有办法在精进了，暂时先手动生成文档留存Git

--- Spring-Boot-MyBatis/src/main/java/com/boot/controller/DocController.java
+++ Spring-Boot-MyBatis/src/main/java/com/boot/controller/DocController.java
@@ -1,8 +1,15 @@
 package com.boot.controller;
 
+import static org.asciidoctor.Asciidoctor.Factory.create;
+
+import java.io.File;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.asciidoctor.Asciidoctor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +24,8 @@ import io.github.swagger2markup.markup.builder.MarkupLanguage;
 @RestController
 @RequestMapping("/boot/v2/docs")
 public class DocController {
+	
+	private Asciidoctor asciidoctor;
 
 	@GetMapping("/generateAscii")
 	public void generateAsciiDocs() throws Exception {
@@ -33,27 +42,7 @@ public class DocController {
                 .withConfig(config)
                 .build()
                 .toFolder(Paths.get("./src/main/resources/docs/asciidoc/generated"));
-    }
-	
-	/**
-     * 生成Markdown格式文档
-     * @throws Exception
-     */
-	@GetMapping("/generateMarkdown")
-    public void generateMarkdownDocs() throws Exception {
-        //    输出Markdown格式
-        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
-                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
-                .withOutputLanguage(Language.ZH)
-                .withPathsGroupedBy(GroupBy.TAGS)
-                .withGeneratedExamples()
-                .withoutInlineSchema()
-                .build();
- 
-        Swagger2MarkupConverter.from(new URL("http://localhost:8060/v2/api-docs"))
-                .withConfig(config)
-                .build()
-                .toFolder(Paths.get("./src/main/resources/docs/markdown/generated"));
+        
     }
     
     /**
@@ -75,6 +64,43 @@ public class DocController {
                 .withConfig(config)
                 .build()
                 .toFile(Paths.get("./src/main/resources/docs/asciidoc/generated/all"));
+        
+        File file = new File("./src/main/resources/docs/asciidoc/generated/all.adoc");
+        
+        if(null == asciidoctor) {
+        	asciidoctor = create();
+        }
+        
+        Map<String, Object> attributes = new HashMap<String, Object>();
+        attributes.put("backend", "html");
+
+        Map<String, Object> options = new HashMap<String, Object>();
+        options.put("in_place", true);
+        options.put("attributes", attributes);
+        
+        String rendered = asciidoctor.renderFile(file, options);
+        System.out.println(rendered);
+    }
+	
+	/**
+     * 生成Markdown格式文档
+     * @throws Exception
+     */
+	@GetMapping("/generateMarkdown")
+    public void generateMarkdownDocs() throws Exception {
+        //    输出Markdown格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+ 
+        Swagger2MarkupConverter.from(new URL("http://localhost:8060/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFolder(Paths.get("./src/main/resources/docs/markdown/generated"));
     }
  
 	/**
