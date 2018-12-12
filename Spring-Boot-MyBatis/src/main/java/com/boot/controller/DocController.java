0fdb06bf112c943a38f8f4b553a440504dc0ae07
杨智文 <yangzhiwen@chalco-steering.com>
2018-12-12 11:25:47 +0800

    生成固定的接口文档

---
+++ Spring-Boot-MyBatis/src/main/java/com/boot/controller/DocController.java
@@ -0,0 +1,106 @@
+package com.boot.controller;
+
+import java.net.URL;
+import java.nio.file.Paths;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.swagger2markup.GroupBy;
+import io.github.swagger2markup.Language;
+import io.github.swagger2markup.Swagger2MarkupConfig;
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.github.swagger2markup.markup.builder.MarkupLanguage;
+
+@RestController
+@RequestMapping("/boot/v2/docs")
+public class DocController {
+
+	@GetMapping("/generateAscii")
+	public void generateAsciiDocs() throws Exception {
+        //    输出Ascii格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+ 
+        Swagger2MarkupConverter.from(new URL("http://localhost:8060/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFolder(Paths.get("src/docs/asciidoc/generated"));
+    }
+	
+	/**
+     * 生成Markdown格式文档
+     * @throws Exception
+     */
+	@GetMapping("/generateMarkdown")
+    public void generateMarkdownDocs() throws Exception {
+        //    输出Markdown格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+ 
+        Swagger2MarkupConverter.from(new URL("http://localhost:8060/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFolder(Paths.get("src/docs/markdown/generated"));
+    }
+    
+    /**
+     * 生成AsciiDocs格式文档,并汇总成一个文件
+     * @throws Exception
+     */
+	@GetMapping("/generateAsciiToFile")
+    public void generateAsciiDocsToFile() throws Exception {
+        //    输出Ascii到单文件
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+ 
+        Swagger2MarkupConverter.from(new URL("http://localhost:8060/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFile(Paths.get("E://项目文件夹/all"));
+    }
+ 
+	/**
+	 * 生成Markdown格式文档,并汇总成一个文件
+	 * 
+	 * @throws Exception
+	 */
+	@GetMapping("/generateMarkdownToFile")
+	public void generateMarkdownDocsToFile() throws Exception {
+		// 输出Markdown到单文件
+		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+				.withMarkupLanguage(MarkupLanguage.MARKDOWN)
+				.withOutputLanguage(Language.ZH)
+				.withPathsGroupedBy(GroupBy.TAGS)
+				.withGeneratedExamples()
+				.withoutInlineSchema().build();
+
+		Swagger2MarkupConverter.from(new URL("http://localhost:8060/v2/api-docs"))
+				.withConfig(config).build()
+				.toFile(Paths.get("E://项目文件夹/all"));
+		
+//		Swagger2MarkupConverter build = Swagger2MarkupConverter.from(new URL("http://localhost:8060/v2/api-docs"))
+//		.withConfig(config).build();
+		
+//		System.out.println(build.toString());
+		
+	}
+	
+}
---
+++ Spring-Boot-MyBatis/src/test/java/com/boot/SwaggerToDoc.java
@@ -0,0 +1,105 @@
+package com.boot;
+
+import java.net.URL;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.github.swagger2markup.GroupBy;
+import io.github.swagger2markup.Language;
+import io.github.swagger2markup.Swagger2MarkupConfig;
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.github.swagger2markup.markup.builder.MarkupLanguage;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerToDoc {
+	
+	/**
+     * 生成AsciiDocs格式文档
+     * @throws Exception
+     */
+    @Test
+    public void generateAsciiDocs() throws Exception {
+        //    输出Ascii格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+ 
+        Swagger2MarkupConverter.from(new URL("http://localhost:8060/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFolder(Paths.get("./docs/asciidoc/generated"));
+    }
+    
+    /**
+     * 生成Markdown格式文档
+     * @throws Exception
+     */
+    @Test
+    public void generateMarkdownDocs() throws Exception {
+        //    输出Markdown格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+ 
+        Swagger2MarkupConverter.from(new URL("http://localhost:8016/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFolder(Paths.get("./docs/markdown/generated"));
+    }
+    
+    /**
+     * 生成AsciiDocs格式文档,并汇总成一个文件
+     * @throws Exception
+     */
+    @Test
+    public void generateAsciiDocsToFile() throws Exception {
+        //    输出Ascii到单文件
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+ 
+        Swagger2MarkupConverter.from(new URL("http://localhost:8016/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFile(Paths.get("./docs/asciidoc/generated/all"));
+    }
+ 
+	/**
+	 * 生成Markdown格式文档,并汇总成一个文件
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void generateMarkdownDocsToFile() throws Exception {
+		// 输出Markdown到单文件
+		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+				.withMarkupLanguage(MarkupLanguage.MARKDOWN)
+				.withOutputLanguage(Language.ZH)
+				.withPathsGroupedBy(GroupBy.TAGS)
+				.withGeneratedExamples()
+				.withoutInlineSchema().build();
+
+		Swagger2MarkupConverter.from(new URL("http://localhost:8060/v2/api-docs"))
+				.withConfig(config).build()
+				.toFile(Paths.get("./docs/markdown/generated/all"));
+	}
+
+}
