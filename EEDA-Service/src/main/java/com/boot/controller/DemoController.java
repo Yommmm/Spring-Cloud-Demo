51704e6b952cf66baaf2c3930a8d2c3515b00e94
杨智文 <yangzhiwen@chalco-steering.com>
2019-04-11 09:38:12 +0800

    增加一个接口

--- EEDA-Service/src/main/java/com/boot/controller/DemoController.java
+++ EEDA-Service/src/main/java/com/boot/controller/DemoController.java
@@ -1,11 +1,14 @@
 package com.boot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,8 @@ public class DemoController {
 	private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
 	
 	private String command = "command";
+	
+	private List<Object> list = new ArrayList<>();
 	
 	@GetMapping("/command")
 	@ApiOperation(value = "心跳获取控制命令", notes="心跳获取控制命令API", produces = "application/json")
@@ -37,9 +42,17 @@ public class DemoController {
 	
 	@PostMapping("/message")
 	@ApiOperation(value = "设备发送消息到数据仓库接口", notes="设备发送消息到数据仓库API", produces = "application/json")
-	public String sendMessage(Map<String, Object> params) {
+	public String sendMessage(@RequestBody Map<String, Object> params) {
+		list.add(params);
 		logger.info("正在传输设备数据： {}", JSONObject.toJSONString(params));
 		return "sucess";
+	}
+	
+	@GetMapping("/message")
+	@ApiOperation(value = "获取数据仓库所有数据的接口", notes="获取数据仓库所有数据 API", produces = "application/json")
+	public String getMessage() {
+		logger.info("正在传输设备数据： {}", JSONObject.toJSONString(this.list));
+		return JSONObject.toJSONString(this.list);
 	}
 	
 }
